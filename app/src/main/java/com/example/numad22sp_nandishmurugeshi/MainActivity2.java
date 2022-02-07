bb38202f0f4e0e8cce4c71db99cb7ca12fa812c2
Nandish Murugeshi <nandish291@live.com>
2022-02-06 19:08:55 -0500

    A2 Completed

--- app/src/main/java/com/example/numad22sp_nandishmurugeshi/MainActivity2.java
+++ app/src/main/java/com/example/numad22sp_nandishmurugeshi/MainActivity2.java
@@ -3,6 +3,7 @@ package com.example.numad22sp_nandishmurugeshi;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
@@ -11,8 +12,13 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        TextView textView = findViewById(R.id.pressed);
+        textView.setText(getString(R.string.pressed," "));
     }
 
     public void onClickButton(View view) {
+        TextView textView = findViewById(R.id.pressed);
+        Button button = findViewById(view.getId());
+        textView.setText(getString(R.string.pressed, button.getText()));
     }
 }
