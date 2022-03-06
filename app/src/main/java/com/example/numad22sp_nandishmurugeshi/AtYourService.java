2c87599d7ab573ece4929c3fdb221895712f7cc8
Nandish Murugeshi <nandish291@live.com>
2022-03-06 13:45:25 -0500

    Added new activity_at_your_service

---
+++ app/src/main/java/com/example/numad22sp_nandishmurugeshi/AtYourService.java
@@ -0,0 +1,14 @@
+package com.example.numad22sp_nandishmurugeshi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AtYourService extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_at_your_service);
+    }
+}
